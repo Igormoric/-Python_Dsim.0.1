fb78c3032e9c43d7485ed2e8126f6202574918dc
Igormoric <109804630+Igormoric@users.noreply.github.com>
2022-07-23 10:59:06 -0300

    Update Dsim.py

--- Dsim.py
+++ Dsim.py
@@ -2,6 +2,7 @@
 
 from matgeo import dist2p	#eu nao sei se ja existe uma funcao para calcular a distancia entre 2 pontos,entao eu criei uma.
 import random
+import time
 
 class pessoa:	#clase q cria pessoa
 	def __init__(self,x,y,numero,d=0):
@@ -12,41 +13,36 @@ class pessoa:	#clase q cria pessoa
 		#self.a=True
 		self.id=numero	#identifica a pessoa
 
-class simu:
-	def __init__(self,tempo,contamidados,populacao,perigod=5,perigoc=5,mapa=100):
+class simu:	#o programa em si
+	def __init__(self,tempo,contamidados,populacao,perigod=5,perigoc=5,mapa=100):	#prepara a simulacao
 		self.populacao=[]	#todas as pessoas
 		self.populacaoc=[]	#todas as pessoas doentes
 		self.tempolimite=tempo	#quanto tempo vai durar a simulacao
 		self.npd=perigod	#chance de se contaminar
 		self.npc=perigoc	#distancia para se contaminar
 		self.limitemap=mapa	#tamanho maximo do mapa
-		for i in range(populacao):
+		for i in range(populacao):	#cria as pessoas na simulacao
 			self.populacao.append(pessoa(random.randint(1,self.limitemap),random.randint(1,self.limitemap),i))
-		for i in range(contamidados):
-			x=random.randint(1,populacao-1)
+		for i in range(contamidados):	#contamina x pessoas na simulacao
+			x=random.randint(1,populacao)
+			while(self.populacao[x] in self.populacaoc):
+				x=random.randint(1,populacao)
 			self.populacao[x].d=self.npc
-			self.populacaoc.append(self.populacao[x])
+			self.populacaoc.append(self.populacao[x])	#uma lista de pessoas contaminadas
 			#self.listamedia()
 
-	def contadoente(self):
-		x=0
+	def listapessoas(self):	#lista a situacao de cada pessoa
 		for i in self.populacao:
-			if(i.d==5):
-				x+=1
-		return x
+			print("cidadao {:4d} em {:2d},{:2d} estado={}".format(i.id+1,i.x,i.y,i.d))
 	
-	def listapessoas(self):
-		for i in self.populacao:
-			print("cidadao {} em {},{} estado={}".format(i.id+1,i.x,i.y,i.d))
-	
-	def listamedia(self):
-		x=self.contadoente()
+	def listamedia(self):	#informa quantas pessoas estao doentes
+		x=len(self.populacaoc)
 		print("{} pessoas doentes de {}".format(x,len(self.populacao)))
       
-	def getvar(self):
-		print("tempo:{}\npessoas:{}\nnivel de perigo:{}\ninfectados:{}".format(self.tempolimite,len(self.populacao),self.npd,self.contadoente()))
+	def getvar(self):	#para rever os valores das variaves
+		print("tempo:{}\npessoas:{}\nnivel de perigo:{}\ninfectados:{}".format(self.tempolimite,len(self.populacao),self.npd,len(self.populacaoc)))
 
-	def pessoamov(self,pessoa):
+	def pessoamov(self,pessoa):	#simula o movimento das pessoas
 		dire=random.randint(1,9)
 		if((dire==7)or(dire==8)or(dire==9)):
 			if(pessoa.x<self.limitemap):
@@ -61,20 +57,22 @@ class simu:
 			if(pessoa.x>1):
 				pessoa.x-=1
         
-	def pessoacontamina(self):
-		for pessoa in self.populacaoc:
+	def pessoacontamina(self):	#simula a contaminacao
+		for pessoa in self.populacaoc:	#primeiro ve se estao proximos o suficiente para contaminar
 			for i in self.populacao:
-				if((dist2p(pessoa.x,pessoa.y,i.x,i.y)<self.npd)):
-					i.d+=2
-		for i in self.populacao:
-                        if(i.d>self.npc-1):
-                                i.d=self.npc
-                                self.populacaoc.append(i)
-                        else:
+				if i not in self.populacaoc:
+					if((dist2p(pessoa.x,pessoa.y,i.x,i.y)<self.npd)):
+						i.d+=2
+		for i in self.populacao:	#depois ve se ficou perto por muito tempo ou muitas pessoas contamidadas
+			if(i.d>self.npc-1):
+				if i not in self.populacaoc:
+					i.d=self.npc
+					self.populacaoc.append(i)
+			else:
                                 if(i.d>1):
                                         i.d-=1
 				
-	def start(self):
+	def start(self):	#inicia a simulacao em si
 #		self.listapessoas()
 		self.getvar()
 		self.listamedia()
@@ -89,17 +87,18 @@ class simu:
                                 break
 
 	def smalltest(self):
-		test=simu(20,5,40,mapa=10)
-		self.getvar()
-		self.listamedia()
+		smalltest=simu(20,1,40,perigoc=1,mapa=10)
+		smalltest.getvar()
+		smalltest.listamedia()
 		print("\n")
-		for d in range(0,self.tempolimite):
-			for i in self.populacao:
-				self.pessoamov(i)
-			self.pessoacontamina()
+		for d in range(0,smalltest.tempolimite):
+			for i in smalltest.populacao:
+				smalltest.pessoamov(i)
+			smalltest.pessoacontamina()
 			print("dia:{} ".format(d+1))
-			self.listapessoas()
-			if(self.populacao==self.populacaoc):
+			smalltest.listapessoas()
+			time.sleep(1)
+			if(smalltest.populacao==smalltest.populacaoc):
                                 break
         
         
